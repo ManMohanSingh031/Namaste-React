df0bbca401a25273e96a9246fd3282f9bf2e0541
Man Mohan Singh <98742502+ManMohanSingh031@users.noreply.github.com>
2024-01-01 16:22:08 +0530

    lesson-2

---
+++ lesson-3/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  // App component
+  return (
+    <div>
+      <h1>Hello, I am form React !</h1>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
