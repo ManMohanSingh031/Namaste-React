427b12b7c490acca9b59f4b7f82f3e3c69faa252
Man Mohan Singh <98742502+ManMohanSingh031@users.noreply.github.com>
2024-01-01 16:21:33 +0530

    lesson-2

---
+++ lesson-2/script.js
@@ -0,0 +1,34 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  // App component
+  return (
+    <div>
+      <h1>Hello, I am form React !</h1>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+/** 
+* Created A Server
+* HMR - Hot Module Replacement
+* File Watcher algorithm C++
+* BUNDLING
+* MINIFY
+* Cleaning our Code
+* Dev abd Production Build
+* Super Fast build algorithm
+* Image Optimization
+* Caching while development
+* Compression
+* Compatble with older version of browser
+* HTTPS on dev
+* port Number
+* Consistent Hashing Algorithm
+* Zero Config
+* Tree Shaking - Removing un-wanted code
+* Transitive Dependencies
+ */
